954921ff78c698ad128c518a124a45e55ee7203e
GHLgh <gluo2@illinois.edu>
2016-05-25 14:33:01 -0500

    finished the first version. Test needed

--- server/login.c
+++ server/login.c
@@ -3,6 +3,9 @@
 
 #include "login.h"
 
+// TODO: protantially needs a mutex for new_user
+
+static char* userinfo;
 // TODO: what if the file is too big??
 // Assume the file provided is user info file
 int map_userinfo(char* filename){
@@ -11,6 +14,8 @@ int map_userinfo(char* filename){
     fprintf(stderr, "Cannot open %s\n", filename);
     return -1;
   }
+  userinfo = malloc(strlen(filename)+1);
+  strcpy(userinfo, filename);
   char* buffer = NULL;
   size_t size = 0;
   ssize_t len = 0;
@@ -25,11 +30,25 @@ int map_userinfo(char* filename){
     realloc(user_array, sizeof(user_info)*user_num);
     user_array[user_num-1].username = strsep(&user, ",");
     user_array[user_num-1].password = strsep(&user, ",");
-    user_array[user_num-1].character_id = atoi(user); 
+    user_array[user_num-1].character_id = atoi(user);
+    user_array[user_num-1].user_status = 0;
     // The new allocated line becomes: username\0password\0ch_id\0
     // So when it is time to free heap memory, free(usrename) should be sufficient
   }
+  fclose(fd);
   return 0;
+}
+
+void unmap_userinfo(){
+  FILE* fd = fopen(userinfo, a);
+  int i = 0;
+  for(; i < user_num; i++){
+    if(user_array[i].user_status)
+      fprintf(fd, "%s,%s,%d\n", user_array[i].username,user_array[i].password,user_array[i].character_id);
+    free(user_array[i].username);
+  }
+  free(user_array);
+  fclose(fd);
 }
 
 // find_user function will take username that client provided and the length of the username
@@ -45,11 +64,54 @@ int find_user(char* username){
   return -1;
 }
 
+// match_password function is used for user auth
+// there are three tries for each login
+// old user: get password temp from user_array
+// TODO: new user: get password temp by asking user for a password
+// then ask user for password to match the password temp
+// on success, return index (new user will enlarge the user_array and return the new index)
+// on failure, return -1
+// TODO: make read from client a function
+int match_password(int index, char* new_user){
+  int i = 0;
+  for(; i < 3; i++){
+    write(fd, PASSWORD, strlen(PASSWORD));
+    char buffer[MSG_SIZE];
+    int len = 0;
+    int num;
+    while (1) {
+      num = read(fd, buffer + len, MSG_SIZE);
+      len += num;
+      if (!num)
+        return -1;
+      if (buffer[len-1] == '\n')
+        buffer[len-1] = 0;
+        break;
+    }
+    if(index == -1){
+      char* user = malloc(strlen(buffer)+strlen(new_user)+6);
+      sprintf(user, "%s,%s,%d", new_user,buffer,-1);
+
+      user_num++;
+      realloc(user_array, sizeof(user_info)*user_num);
+      user_array[user_num-1].username = strsep(&user, ",");
+      user_array[user_num-1].password = strsep(&user, ",");
+      user_array[user_num-1].character_id = atoi(user);
+      user_array[user_num-1].user_status = 1;
+      return user_num-1;
+    }
+    else if(strcmp(buffer, user_array[index].password)==0)
+      return index;
+  }
+  return -1;
+}
+
 int login(int fd){
   char buffer[MSG_SIZE];
   int len = 0;
   int num;
   int user_status = -1; // -1: username does not exist / mismatch. Otherwise, corresponding position in user_array
+  char* new_user = NULL;
   while(user_status == -1){
     write(fd, USERNAME, strlen(USERNAME));
     while (1) {
@@ -66,6 +128,9 @@ int login(int fd){
       return -1;
 
     user_status = find_user(buffer);
-    // TODO: check password / create new user if not found
+    if(user_status == -1)
+      write(fd, NEWUSER, strlen(NEWUSER));
+    user_status = match_password(user_status, buffer);
   }
+  return user_array[user_status].character_id;
 }
--- server/login.h
+++ server/login.h
@@ -2,11 +2,13 @@
 // TODO: how to make this as a file (easier to modify??)
 #define USERNAME "Please enter your username\n"
 #define PASSWORD "Please enter your password\n"
+#define NEWUSER  "Username does not exist, creating new character...\n"
 
-struct _user_info{
+typedef struct _user_info{
   char* username;
   char* password;
   int character_id;
+  int user_status; // 0: if old user, 1: if new user
 }user_info;
 
 // It will be used as a pointer to the first element in user_info array
@@ -19,8 +21,13 @@ int user_num;
 // Return 0 on success. Otherwise, return -1
 int map_userinfo(char* filename);
 
+// A function to clean up the allocated resource and update new user to 
+// the original userinfo file
+// This functgion should be called when the server is going to shut down
+void unmap_userinfo();
+
 // This function is called when user sucessfully connect to the server 
 // It is similar to processClient function, but specify for user login
-// return -1 if user exit, otherwise return file descriptor
+// return -1 if user exit, otherwise return character_id
 // (corresponding to the cell in character array)
 int login(int fd);
